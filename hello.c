46e0d967b1b91fbe14f509386c50d3cb5cff6725
LuanaTaietti <lutaietti@gmail.com>
2025-08-26 14:24:32 +0000

    Declaração de variável - aula 26/08/2025

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h> //é a biblioteca que permite o uso do printf
 
 int main () {
-    printf("Olá Mundo!!!");
+    int idade; //declaração de variáveis 
+
+    idade = 23; //foi atribuido o valor 23 a variável idade | ou pode ser colocado na frente da variável idade caso eu não vá fazer o uso dessa variável agora 
+    
+    printf("Olá Mundo!!!\n"); //printf permite que os caracteres entre ("") apareçam na tela
+    printf("Idade: %d\n", idade); // \n indica quebra de linha 
     return 0;
 }
